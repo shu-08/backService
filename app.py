2e779f91333278a14bb56da8b2153fd26360cc06
bpwooooooooooorks-maru <maruyama@bpworks.jp>
2025-01-31 19:30:29 +0900

    Initial commit3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # すべてのオリジンを許可
 
-@app.route("/data")
+@app.route("/")
 def get_data():
     return jsonify({"message": "Hello from Flask!"})
 
