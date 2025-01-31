dc0db97756ea04f3642914e84b9733f6ae54a1fe
shu-08 <shu.maruyama08@gmail.com>
2025-01-31 20:30:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+from flask_cors import CORS  # flask_corsをインポート
+
 import yt_dlp
 import os
 
 app = Flask(__name__)
 
-# 動画の保存先
+# CORSを有効にする（特定のオリジンのみ許可）
+CORS(app, resources={r"/*": {"origins": "https://front-service.vercel.app"}})
+
 DOWNLOAD_FOLDER = 'downloads'
 if not os.path.exists(DOWNLOAD_FOLDER):
     os.makedirs(DOWNLOAD_FOLDER)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
 def download():
     url = request.form['url']
-    
     try:
         ydl_opts = {
             'format': 'best',
@@ -20,7 +27,6 @@ def download():
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             video_info = ydl.extract_info(url, download=True)
-            # ダウンロードされたファイルのフルパスを生成
             file_path = os.path.join(DOWNLOAD_FOLDER, f"{video_info['title']}.{video_info['ext']}")
         
         return render_template('index.html', download_link=file_path)
